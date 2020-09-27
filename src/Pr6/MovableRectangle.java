b533257346e0db2494764f40a7ec3020d9156dcc
kelasov <kelasovg@mail.ru>
2020-09-27 23:51:10 +0300

    init

---
+++ src/Pr6/MovableRectangle.java
@@ -0,0 +1,33 @@
+package Pr6;
+
+public class MovableRectangle implements Movable{
+    private MovablePoint topLeft;
+    private MovablePoint bottomRight;
+
+    public MovableRectangle(int x1, int y1, int x2, int y2, int xSpeed1, int ySpeed1, int xSpeed2, int ySpeed2){
+        topLeft = new MovablePoint(x1, y1, xSpeed1, ySpeed1);
+        bottomRight = new MovablePoint(x2, y2, xSpeed2, ySpeed2);
+    }
+
+    @Override
+    public String toString() {
+        return "MovableRectangle{" +
+                "topLeft=" + topLeft +
+                ", bottomRight=" + bottomRight +
+                '}';
+    }
+
+    @Override
+    public void moveUp() {
+    }
+    @Override
+    public void moveDown() {
+    }
+    @Override
+    public void moveLeft() {
+    }
+    @Override
+    public void moveRight() {
+    }
+
+}
